87351f46b31a44e81e3923e09fb30f0ec6939eb3
Cyntya López <cyntyask8@gmail.com>
2022-08-23 16:44:05 -0500

    Clases y Objetos Java con Vic

--- java/POOJava/src/EjemploSaludo.java
+++ java/POOJava/src/EjemploSaludo.java
@@ -1,2 +1,21 @@
 public class EjemploSaludo {
+    public static void main(String[] args) {
+
+
+        //Declaración: Creamos un objeto de la misma referencia
+        Saludo objSaludo;
+
+        //Construcción: Despues le asignamos memoria:
+        objSaludo = new Saludo(); //new Saludo() es el metodo constructor
+
+        //Invocación a la clase saludar
+        objSaludo.saludar(); //Esta es la instancia: la que lo llama
+
+        //Otra forma de reutilizar la misma clase con diferentes objetos
+        Saludo objSaludo0 = new Saludo();
+
+        System.out.println(objSaludo.saludar0());
+        System.out.println("Este es otro objeto: -> " + objSaludo0.saludar0());
+
+    }
 }
--- java/POOJava/src/Saludo.java
+++ java/POOJava/src/Saludo.java
@@ -1,2 +1,10 @@
 public class Saludo {
+
+        void saludar(){
+            System.out.println("Hola Java OOP");
+        }
+        String saludar0(){
+            String textoS = "Hola mundo Java OOP";
+            return textoS;
+        }
 }
