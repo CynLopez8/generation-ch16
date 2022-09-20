f482ba0d561a86e065add27525cbf5e89fdbea6c
Cyntya López <cyntyask8@gmail.com>
2022-09-20 09:17:18 -0500

    Seguridad de API en Spring

--- 7-spring/SpringJpaHibernate/SpringJpa/src/main/java/com/generation/cell/controllers/UsuarioController.java
+++ 7-spring/SpringJpaHibernate/SpringJpa/src/main/java/com/generation/cell/controllers/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,12 +13,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 import com.generation.cell.models.UsuarioModel;
 import com.generation.cell.services.UsuarioService;
 
 @RestController
 @RequestMapping("/usuario")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE,
+        RequestMethod.PUT })
 
 
 public class UsuarioController {
